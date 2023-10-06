94a40ec964235533a2d0e620f9e37ebf0a14d9b0
MingbinY <Mingbin.Yang@outlook.com>
2023-10-06 03:49:04 -0400

    basic health system

--- Project-Balancing/Assets/Scripts/Stats/BasicHealthManager.cs
+++ Project-Balancing/Assets/Scripts/Stats/BasicHealthManager.cs
@@ -8,7 +8,22 @@ public class BasicHealthManager : MonoBehaviour
     public int health;
 
     public float regenerateRate = 0f;
+    
+    public virtual void Start()
+    {
+        health = maxHealth;
+    }
 
-    public virtual void Death() { }
-    public virtual void TakeDamage(int damage) { }
+    public virtual void Update()
+    {
+        if (health <= 0)
+        {
+            Death();
+        }
+    }
+    public virtual void Death() { Destroy(gameObject); }
+    public virtual void TakeDamage(int damage) { 
+        Debug.Log(gameObject.name + "Taking Damage of " + damage);
+        health -= damage;
+    }
 }
