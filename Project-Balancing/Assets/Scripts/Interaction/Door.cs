8c3eea67a67d2b3d45138bd9bf92348d16bca6e5
MingbinY <Mingbin.Yang@outlook.com>
2023-10-15 20:12:06 -0400

    Door open close (Trigger) script

--- Project-Balancing/Assets/Scripts/Interaction/Door.cs
+++ Project-Balancing/Assets/Scripts/Interaction/Door.cs
@@ -1,22 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Door : MonoBehaviour
 {
     public bool isLocked = false;
 
+    [SerializeField] GameObject movingPart;
+    [SerializeField] Vector3 closePosition;
+    [SerializeField] Vector3 openPosition;
+    [SerializeField] Quaternion closeRotation;
+    [SerializeField] Quaternion openRotation;
+
+    bool isOpen = false;
+    [SerializeField] float moveSpeed = 1f;
+    private void Awake()
+    {
+        closePosition = movingPart.transform.localPosition;
+        closeRotation = movingPart.transform.rotation;
+    }
+
+    private void Update()
+    {
+       if (isOpen)
+        {
+            movingPart.transform.localPosition = Vector3.Lerp(movingPart.transform.localPosition, openPosition, moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            movingPart.transform.localPosition = Vector3.Lerp(movingPart.transform.localPosition, closePosition, moveSpeed * Time.deltaTime);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (isLocked)
+        if (isLocked || !other.GetComponent<PlayerManager>())
             return;
+
+        isOpen = true;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (isLocked)
+        if (isLocked || !other.GetComponent<PlayerManager>())
             return;
+
+        isOpen = false;
     }
-
-
 }
